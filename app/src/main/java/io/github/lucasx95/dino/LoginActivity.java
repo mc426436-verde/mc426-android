c6f860af571c1e05db61a464a51839054124d17b
lucasx95 <andlaax95@gmail.com>
2016-05-19 19:27:53 -0300

    Initial commit
    Signed-off-by: lucasx95 <andlaax95@gmail.com>

---
+++ app/src/main/java/io/github/lucasx95/dino/LoginActivity.java
@@ -0,0 +1,17 @@
+package io.github.lucasx95.dino;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
