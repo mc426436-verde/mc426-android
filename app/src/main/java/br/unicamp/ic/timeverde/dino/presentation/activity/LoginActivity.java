649f525105358354fc50c71e06a2d19ba02eaf2c
lucasx95 <andlaax95@gmail.com>
2016-05-22 04:24:08 -0300

    Icone e ButterKnife com Binds da Login
    A MainActivity tmabém foi adicionada vazia.
    
    Signed-off-by: lucasx95 <andlaax95@gmail.com>

--- app/src/main/java/br/unicamp/ic/timeverde/dino/presentation/activity/LoginActivity.java
+++ app/src/main/java/br/unicamp/ic/timeverde/dino/presentation/activity/LoginActivity.java
@@ -1,17 +1,39 @@
 package br.unicamp.ic.timeverde.dino.presentation.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import br.unicamp.ic.timeverde.dino.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.login_button)
+    Button mBtLogin;
+
+    @BindView(R.id.login_user_email_input)
+    EditText mEtLoginInput;
+
+    @BindView(R.id.login_user_password_input)
+    EditText mEtPasswordInput;
+
+    @OnClick(R.id.login_button)
+    public void doLogin(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
 }
---
+++ app/src/main/java/br/unicamp/ic/timeverde/dino/presentation/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package br.unicamp.ic.timeverde.dino.presentation.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.unicamp.ic.timeverde.dino.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
