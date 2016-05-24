06adf1dd5006feed286c205f7f1e74ddd763efe9
wilson.martins <martins.wilson17@gmail.com>
2016-05-23 23:03:32 -0300

    Room Card
    Layout do card dos cômodos
    
    Signed-off-by: wilson.martins <martins.wilson17@gmail.com>

---
+++ app/src/main/java/br/unicamp/ic/timeverde/dino/presentation/fragment/RoomFragment.java
@@ -0,0 +1,25 @@
+package br.unicamp.ic.timeverde.dino.presentation.fragment;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.widget.ProgressBar;
+
+import br.unicamp.ic.timeverde.dino.R;
+
+/**
+ * Created by wilson on 19/05/16.
+ */
+public class RoomFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.fragment_room);
+
+
+    }
+
+}
