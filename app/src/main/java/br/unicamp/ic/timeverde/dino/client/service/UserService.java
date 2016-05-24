38a556093de2777ccb502386c0b59885d1fa3af6
lucasx95 <andlaax95@gmail.com>
2016-05-23 23:58:43 -0300

    Adicionando list-item do device fragment
    Signed-off-by: lucasx95 <andlaax95@gmail.com>

--- app/src/main/java/br/unicamp/ic/timeverde/dino/client/service/UserService.java
+++ app/src/main/java/br/unicamp/ic/timeverde/dino/client/service/UserService.java
@@ -4,9 +4,10 @@ package br.unicamp.ic.timeverde.dino.client.service;
 import br.unicamp.ic.timeverde.dino.model.Token;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface UserService {
 
-    @GET("/oauth/token")
+    @POST("/oauth/token")
     Call<Token> authenticate(String username, String password);
 }
