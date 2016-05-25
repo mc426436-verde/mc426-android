d10d2b559b7e86eda56f1f0b4396761941798213
lucasx95 <andlaax95@gmail.com>
2016-05-25 19:52:33 -0300

    Adicionando adapter para RoomFragment
    Signed-off-by: lucasx95 <andlaax95@gmail.com>

---
+++ app/src/main/java/br/unicamp/ic/timeverde/dino/adapter/RoomFragmentAdapter.java
@@ -0,0 +1,66 @@
+package br.unicamp.ic.timeverde.dino.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.unicamp.ic.timeverde.dino.R;
+
+/**
+ * Adapter para o RecyclerView do Fragment de Devices
+ */
+public class RoomFragmentAdapter extends RecyclerView.Adapter<RoomFragmentAdapter.ViewHolder> {
+    private ArrayList<String> mDataset;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mItemRoom;
+        public TextView mItemOnNumber;
+        public TextView mItemOffNumber;
+        public ImageView mItemOnIcon;
+        public ImageView mItemOffIcon;
+
+        public ViewHolder(View v) {
+            super(v);
+            mItemRoom = (TextView) v.findViewById(R.id.room_fragment_card_text);
+            mItemOnNumber = (TextView) v.findViewById(R.id.room_fragment_turn_on_light_text);
+            mItemOffNumber = (TextView) v.findViewById(R.id.room_fragment_turn_off_light_text);
+            mItemOnIcon = (ImageView) v.findViewById(R.id.room_fragment_turn_on_light_image);
+            mItemOffIcon = (ImageView) v.findViewById(R.id.room_fragment_turn_off_light_image);
+        }
+    }
+
+    public RoomFragmentAdapter(ArrayList<String> mDataset) {
+        //TODO recebe e seta o data set
+        this.mDataset = mDataset;
+    }
+
+    @Override
+    public RoomFragmentAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                             int viewType) {
+        final View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_room_fragment, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // TODO Bind do view
+       holder.mItemRoom.setText(mDataset.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+}
+
+
--- app/src/main/java/br/unicamp/ic/timeverde/dino/presentation/fragment/RoomFragment.java
+++ app/src/main/java/br/unicamp/ic/timeverde/dino/presentation/fragment/RoomFragment.java
@@ -2,27 +2,52 @@ package br.unicamp.ic.timeverde.dino.presentation.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import br.unicamp.ic.timeverde.dino.R;
+import br.unicamp.ic.timeverde.dino.adapter.RoomFragmentAdapter;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RoomFragment extends Fragment {
 
+    @BindView(R.id.room_recycler_view)
+    RecyclerView mRecyclerView;
+
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private RoomFragmentAdapter mRoomFragmentAdapter;
+
 
     public RoomFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_room, container, false);
+
+        final View view = inflater.inflate(R.layout.fragment_room, container, false);
+        ButterKnife.bind(this, view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mRoomFragmentAdapter = new RoomFragmentAdapter(new ArrayList<>(Arrays.asList("Lâmpada","Outra Lâmpada")));
+        mRecyclerView.setAdapter(mRoomFragmentAdapter);
+
+        return view;
     }
 }
